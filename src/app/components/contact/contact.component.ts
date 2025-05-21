70e717f2fdd54f22f14a6ca985bf35a932d1dab0
Rob Kazirut <robert.kazirut@gmail.com>
2025-05-21 17:02:40 -0400

    some form validation, submission states

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    CommonModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
@@ -20,14 +30,36 @@ export class ContactComponent {
     service: '',
     message: '',
   };
+
   isSubmitting = false;
+  tooltipVisible = signal(false);
+
+  @ViewChild('sendMsgBtn') sendMsgBtn!: ElementRef;
 
   constructor(private http: HttpClient) {}
+
+  ngAfterViewInit() {
+    if (this.sendMsgBtn && this.sendMsgBtn.nativeElement) {
+      this.sendMsgBtn.nativeElement.addEventListener(
+        'mouseover',
+        (event: MouseEvent) => {
+          if (this.sendMsgBtn.nativeElement.disabled) {
+            this.tooltipVisible.set(true);
+          }
+        }
+      );
+      this.sendMsgBtn.nativeElement.addEventListener(
+        'mouseout',
+        (event: MouseEvent) => {
+          this.tooltipVisible.set(false);
+        }
+      );
+    }
+  }
 
   submitForm(event: Event) {
     event.preventDefault();
     this.isSubmitting = true;
-
     const serverUrl = environment.SERVER_URL || '';
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
